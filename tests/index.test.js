6e60eab9b12d0086c257b2cb4da8ecd0a4babee9
João Batista <batistadasilvaalvesjoao@gmail.com>
2021-07-25 13:55:31 -0300

    Adiciona teste

---
+++ tests/index.test.js
@@ -0,0 +1,9 @@
+const valida = require('../index');
+
+test("Retorna true se cpf válido", ()=>{
+    expect(valida.cpf("343.454.670-71")).toBe(true);
+});
+
+test("Retorna false se cpf não for válido", ()=>{
+    expect(valida.cpf("123123123123")).toBe(false);
+});
